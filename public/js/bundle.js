d742bc5316e421f873ceaa3da82c2f9752f5ec1c
Gaurav07-droid <itsgaurav026@gmail.com>
2022-05-30 11:38:35 +0530

    added changes

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -11855,7 +11855,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51477" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64664" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
