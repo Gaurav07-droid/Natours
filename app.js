16499b03bb37b7defe40c10d1614c03bea089959
Gaurav07-droid <itsgaurav026@gmail.com>
2022-05-31 14:43:28 +0530

    stripe webhooks  implementation

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
 
 app.post(
   '/webhook-checkout',
-  bodyParser.raw({ type: 'application/json' }),
+  express.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
